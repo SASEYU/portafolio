27777c5c4025896478d3e57a97b98a3751772587
Samuel <samuelsegur@gmail.com>
2025-03-01 18:04:11 -0500

    actualizacion de js

--- scrip.js
+++ scrip.js
@@ -1,6 +1,29 @@
 
-//Menú Hamburguesa//
+// Menú Hamburguesa
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.querySelector(".menu__items").classList.toggle("active");
 });
+
+// Cerrar el menú y hacer scroll al hacer clic en cualquier enlace
+document.querySelectorAll(".menu__items a").forEach(item => {
+    item.addEventListener("click", function(event) {
+        const targetId = this.getAttribute("href");
+
+        // Verificar que el href es un ID válido (evitar '#')
+        if (targetId && targetId.startsWith("#") && targetId.length > 1) {
+            event.preventDefault(); // Evita el comportamiento predeterminado
+
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+
+        // Cerrar el menú hamburguesa después del clic
+        document.querySelector(".menu__items").classList.remove("active");
+    });
+});
+
+
+
 
